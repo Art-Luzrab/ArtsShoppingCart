9ec8d1161cf9819fee8d059ab59de75fb6b2d2c3
Arthur <arthur91021@gmail.com>
2024-12-31 07:50:47 -0800

    added images folder with groceries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 const market = [
-  { id: 1, name: "Tomato", price: 0.5, category: "Vegetable", inStock: true },
+  { id: 1, name: "Tomato", price: 0.5, category: "Vegetable", inStock: true, img:  },
   { id: 2, name: "Apple", price: 0.6, category: "Vegetable", inStock: true },
   { id: 3, name: "Bread", price: 2, category: "Wheat", inStock: true },
-  { id: 4, name: "Eggs", price: 4, category: "Vegetable", inStock: true },
+  { id: 4, name: "Eggs", price: 4, category: "Protein", inStock: true },
   { id: 5, name: "Milk", price: 3, category: "Vegetable", inStock: true },
 ];
 
 function App() {
-  return <div>Here will lie a shopping cart</div>;
+  return (
+    <div>
+      <GroceryStore />
+    </div>
+  );
+}
+
+function GroceryStore() {
+  const groceries = market.map((grocery) => <li>{grocery.name}</li>);
+
+  return (
+    <div className="market">
+      <ul>{groceries}</ul>
+    </div>
+  );
+}
+
+function Grocery() {
+  return ()
 }
 
 export default App;
