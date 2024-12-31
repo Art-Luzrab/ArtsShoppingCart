c0145b2fb3a579075e11e1e7cef6fbd762d3283d
Arthur <arthur91021@gmail.com>
2024-12-31 07:34:44 -0800

    added market data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+const market = [
+  { id: 1, name: "Tomato", price: 0.5, category: "Vegetable", inStock: true },
+  { id: 2, name: "Apple", price: 0.6, category: "Vegetable", inStock: true },
+  { id: 3, name: "Bread", price: 2, category: "Wheat", inStock: true },
+  { id: 4, name: "Eggs", price: 4, category: "Vegetable", inStock: true },
+  { id: 5, name: "Milk", price: 3, category: "Vegetable", inStock: true },
+];
+
 function App() {
   return <div>Here will lie a shopping cart</div>;
 }
