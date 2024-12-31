f6b2115047a76094f6fa6ee73530e9a8b25e9d31
Arthur <arthur91021@gmail.com>
2024-12-31 08:41:27 -0800

    market rendered with title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
+import apple from "./images/apple.webp";
+import bread from "./images/bread.jpg";
+import eggs from "./images/eggs.webp";
+import milk from "./images/milk.webp";
+import tomato from "./images/tomato.jpeg";
 const market = [
-  { id: 1, name: "Tomato", price: 0.5, category: "Vegetable", inStock: true, img:  },
-  { id: 2, name: "Apple", price: 0.6, category: "Vegetable", inStock: true },
-  { id: 3, name: "Bread", price: 2, category: "Wheat", inStock: true },
-  { id: 4, name: "Eggs", price: 4, category: "Protein", inStock: true },
-  { id: 5, name: "Milk", price: 3, category: "Vegetable", inStock: true },
+  {
+    id: 1,
+    name: "Tomato",
+    price: 0.5,
+    category: "Vegetable",
+    inStock: true,
+    photo: tomato,
+  },
+  {
+    id: 2,
+    name: "Apple",
+    price: 0.6,
+    category: "Vegetable",
+    inStock: true,
+    photo: apple,
+  },
+  {
+    id: 3,
+    name: "Bread",
+    price: 2,
+    category: "Wheat",
+    inStock: true,
+    photo: bread,
+  },
+  {
+    id: 4,
+    name: "Eggs",
+    price: 4,
+    category: "Protein",
+    inStock: false,
+    photo: eggs,
+  },
+  {
+    id: 5,
+    name: "Milk",
+    price: 3,
+    category: "Dairy",
+    inStock: true,
+    photo: milk,
+  },
 ];
 
 function App() {
@@ -15,17 +55,38 @@ function App() {
 }
 
 function GroceryStore() {
-  const groceries = market.map((grocery) => <li>{grocery.name}</li>);
+  const groceries = market.map((item) => (
+    <Grocery
+      id={item.id}
+      name={item.name}
+      price={item.price}
+      category={item.category}
+      inStock={item.inStock}
+      photo={item.photo}
+    />
+  ));
 
   return (
-    <div className="market">
-      <ul>{groceries}</ul>
-    </div>
+    <>
+      <div className="market-container">
+        <h1>Welcome To Arthur's Market</h1>
+        <div className="market">
+          <ul>{groceries}</ul>
+        </div>
+      </div>
+    </>
   );
 }
 
-function Grocery() {
-  return ()
+function Grocery({ id, name, price, category, inStock, photo }) {
+  return (
+    <div className="grocery">
+      <img className="picture" src={photo} alt={name} />
+      <p>{name}</p>
+      <p>${price}</p>
+      <p>{inStock ? "In Stock" : "Out of Stock"}</p>
+    </div>
+  );
 }
 
 export default App;
