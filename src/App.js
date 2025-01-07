ad842a2049bb73551648d9a896aab5f976f36343
Arthur <arthur91021@gmail.com>
2025-01-07 12:34:16 -0800

    Now adding to cart works with correct total calculations!

--- src/App.js
+++ src/App.js
@@ -142,6 +142,13 @@ function Grocery({
 }
 
 function Cart({ cart }) {
+  const subTotal = cart.reduce((acc, curr) => {
+    return Number(acc + curr.price * curr.amountOrdered);
+  }, 0);
+
+  const taxTotal = subTotal * 0.1;
+  const total = subTotal + taxTotal;
+
   return (
     <>
       <div className="cart-container">
@@ -150,7 +157,7 @@ function Cart({ cart }) {
         </div>
         <ul className="ul-cart-items">
           {cart.map((grocery) => (
-            <li className="word-separator cart-item">
+            <li className="word-separator cart-item" key={grocery.id}>
               <p>{`${grocery.name} (${grocery.amountOrdered})`}</p>
               <p>{`$${grocery.price * grocery.amountOrdered}`}</p>
             </li>
@@ -166,13 +173,13 @@ function Cart({ cart }) {
 
         <div className="total">
           <div className="word-separator">
-            <p>Subtotal:</p> <p>$8.00</p>
+            <p>Subtotal:</p> <p>{`$${subTotal.toFixed(2)}`}</p>
           </div>
           <div className="word-separator">
-            <p>Tax:</p> <p>$0.80</p>
+            <p>Tax:</p> <p>{`$${taxTotal.toFixed(2)}`}</p>
           </div>
           <div className="word-separator">
-            <p>Total:</p> <p>$8.80</p>
+            <p>Total:</p> <p>{`$${total.toFixed(2)}`}</p>
           </div>
         </div>
       </div>
